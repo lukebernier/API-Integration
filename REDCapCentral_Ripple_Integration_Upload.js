7978e39e41fc1efcc6b0b91c5b48f4ea5bc9aed5
Luke Bernier <103796468+lukebernier@users.noreply.github.com>
2024-07-16 12:38:02 -0400

    Update REDCapCentral_Ripple_Integration_Upload.js

--- REDCapCentral_Ripple_Integration_Upload.js
+++ REDCapCentral_Ripple_Integration_Upload.js
@@ -8,7 +8,7 @@ date_string_fname = get_date_string(1);
 
 // call newman.run to pass `options` object and wait for callback
 newman.run({
-    collection: require('./RCC_to_Ripple.postman_collection.json'),
+    collection: require('./RCC_to_Ripple_Updated.postman_collection.json'),
     reporters: 'cli'
 }).on('request', (error, data) => {
     if (error) {
@@ -27,7 +27,7 @@ newman.run({
     var aoa_study = [];
     var headers_study = ['globalId', 'cv.dem_b_complete', 'cv.dem_c_complete', 'cv.dem_cg_complete', 
     'cv.dem_addr_hx_complete', 'cv.medical_history_of_the_child_complete', 'cv.medical_history_of_the_biological_family_complete', 
-    'cv.to_be_completed_forms','event.child_anthropometry.completed',
+    'cv.forms_to_be_completed','event.child_anthropometry.completed',
     'event.child_anthropometry.completedDate', 'event.mom_anthropometry.completed','event.mom_anthropometry.completedDate', 'importType'];
 
     aoa_study.push(headers_study);
@@ -50,10 +50,13 @@ newman.run({
     var dem_addr;
     var mhc;
     var mhb;
+    var head;
     var child_an;
     var mom_an;
     var first_row_child = true;
     var first_row_mom = true;
+
+    var date_mom_anthro; var date_child_anthro;
 
     var ids_in_study = ['T125', 'T126']; //TODO : LOAD FROM FILE
     
@@ -62,25 +65,28 @@ newman.run({
         
         if (contentJSON[i].redcap_event_name == 'preearly_arm_1') {  // change to a05mos_arm_1
             forms = [contentJSON[i].prg_subuse_r_complete, contentJSON[i].prg_msuppsfbf_complete, contentJSON[i].cnp_prams_complete, 
-                contentJSON[i].cnp_weds_complete, contentJSON[i].cnp_pdep8a_complete, contentJSON[i].cnp_pdep_cat_complete, 
-                contentJSON[i].cnp_panx8a_complete, contentJSON[i].cnp_panx_cat_complete, contentJSON[i].cnp_ntpss10_complete, 
-                contentJSON[i].cnp_pes4a_complete, contentJSON[i].cnp_pes_cat_complete, contentJSON[i].cnp_pinfs4a_complete, 
-                contentJSON[i].cnp_pinfs_cat_complete, contentJSON[i].cnp_pinstrs4a_complete, contentJSON[i].cnp_pinstrs_cat_complete, 
-                contentJSON[i].cgw_pgls5a_complete, contentJSON[i].cgw_pgls_cat_complete, contentJSON[i].cgw_sha_complete, 
-                contentJSON[i].bpe_heshs_c_complete, contentJSON[i].chb_ifp_complete, contentJSON[i].chb_shinf_complete];
+                contentJSON[i].cnp_weds_complete, contentJSON[i].cnp_pdep8a_complete, contentJSON[i].cnp_panx8a_complete, 
+                contentJSON[i].cnp_ntpss10_complete, contentJSON[i].cnp_pes4a_complete, contentJSON[i].cnp_pinfs4a_complete, 
+                contentJSON[i].cnp_pinstrs4a_complete, contentJSON[i].cgw_pgls5a_complete, contentJSON[i].cgw_sha_complete, 
+                contentJSON[i].bpe_heshs_c_complete, contentJSON[i].chb_shinf_complete, contentJSON[i].hhx_chl_complete, 
+                contentJSON[i].hse_csi4_complete];
 
+            d = get_date_string(0);
 
             if (contentJSON[i].dem_dem_b_complete == 2){dem_b = 'Yes';}else{dem_b = 'No';}
             if (contentJSON[i].dem_dem_c_complete == 2){dem_c = 'Yes';}else{dem_c = 'No';}
             if (contentJSON[i].dem_dem_cg_complete == 2){dem_cg = 'Yes';}else{dem_cg = 'No';}
             if (contentJSON[i].dem_addr_hx_complete == 2){dem_addr = 'Yes';}else{dem_addr = 'No';}
             if (contentJSON[i].prg_pa_complete == 2){mom_an = 'TRUE';}else{mom_an = 'FALSE';} //pregnancy anthro - change to 0-5
-            if (contentJSON[i].cph_cape_0_23m_complete == 2){child_an = 'TRUE';}else{child_an = 'FALSE';}
-            if (contentJSON[i].hhx_mh_c_complete == 2){mhc = 'Yes';}else{mhc = 'No';}
-            if (contentJSON[i].hhx_chl_complete == 2){mhb = 'Yes';}else{mhb = 'No';}
+            if (contentJSON[i].cph_clwt_0_23m_complete == 2){child_an = 'TRUE';}else{child_an = 'FALSE';}
+            if (contentJSON[i].hhx_mh2_c_complete == 2){mhc = 'Yes';}else{mhc = 'No';}
+            if (contentJSON[i].hhx_mh2_f_complete == 2){mhb = 'Yes';}else{mhb = 'No';}
+            if (contentJSON[i].cph_head_complete == 2){head = 'TRUE';}else{head = 'FALSE';}
+            if (mom_an == 'TRUE'){date_mom_anthro = d;}else{date_mom_anthro = '';}
+            if (child_an == 'TRUE'){date_child_anthro = d;}else{date_child_anthro = '';}
             
-            d = get_date_string(0);
-            
+
+
             //global file aoa
             if (ids_in_study.includes(contentJSON[i].record_id)) {
 
@@ -102,7 +108,7 @@ newman.run({
                     
                     temp_study_child =  [contentJSON[i].record_id, dem_b, dem_c, 
                     dem_cg, dem_addr, mhc, mhb, get_forms_tbc(forms), 
-                    child_an, d, mom_an, d, '']; //TODO: Add 0-5 month study import code
+                    child_an, date_child_anthro, mom_an, date_mom_anthro, '']; //TODO: Add 0-5 month study import code
                     
                     first_row_child = false;
                 }
@@ -110,7 +116,7 @@ newman.run({
                     
                     temp_study_child = [contentJSON[i].record_id, dem_b, dem_c, 
                     dem_cg, dem_addr, mhc, mhb, get_forms_tbc(forms), 
-                    child_an, d, mom_an, d];
+                    child_an, date_child_anthro, mom_an, date_mom_anthro];
                 }
                 aoa_study.push(temp_study_child);
             }
@@ -124,8 +130,8 @@ newman.run({
     xl.utils.book_append_sheet(workbook_study, aoaTest_study, "data");
     xl.utils.book_append_sheet(workbook_global, aoaTest_global, "data");
 
-    var newFileName_study = "test_" + date_string_fname + "_study" + ".xlsx";
-    var newFileName_global = "test_" + date_string_fname + "_global" + ".xlsx";
+    var newFileName_study = "Log Files/log_" + date_string_fname + "_study" + ".xlsx";
+    var newFileName_global = "Log Files/log_" + date_string_fname + "_global" + ".xlsx";
 
     //Save one version as a way to look at what happened for every run
     xl.writeFile(workbook_study, newFileName_study, {raw:false, cellDates: true,dateNF:'mm/dd/yyyy'});
@@ -133,21 +139,18 @@ newman.run({
 
     //Save another version for the actual import - I cannot edit the .json file to update the file path so
     // every time time this runs it needs to have the same name for the excel file import
-    xl.writeFile(workbook_study, "test_study.xlsx", {raw:false, cellDates: true,dateNF:'mm/dd/yyyy'});
-    xl.writeFile(workbook_global, "test_global.xlsx", {raw:false, cellDates: true,dateNF:'mm/dd/yyyy'});
+    xl.writeFile(workbook_study, "Upload Files/upload_study.xlsx", {raw:false, cellDates: true,dateNF:'mm/dd/yyyy'});
+    xl.writeFile(workbook_global, "Upload Files/upload_global.xlsx", {raw:false, cellDates: true,dateNF:'mm/dd/yyyy'});
     
 })
 
 function get_forms_tbc(forms) {
 
-    //TODO: need to change keys to match what ripple ends up being
-    keys = {"key":["substance_use_recall | ", "supplements_short_form_breastfeeding | ", "PRAMS | ", 
-    "the_everyday_discrimination_scale | ", "promis_depression_8a | ", "promis_depression_cat | ", "promis_anxiety_8a | ", 
-    "promis_anxiety_cat | ", "percieved_stress_scale | ", "promis_emotional_support_4a | ", "promis_emotional_support_cat | ", 
-    "promis_informational_support_4a | ", "promis_informational_support_cat | ", "promis_instrumental_support_4a | ", 
-    "promis_instrumental_support_cat | ", "promis_general_life_satisfaction_5a | ", "promis_general_life_satisfaction_cat | ", 
-    "sleep_health_of_adults | ", "household_exposure_to_secondhand_smoke_current | ", "infant_feeding_practices | ", 
-    "sleep_health_of_infants | "]};
+    keys = {"key":["substance_use_recall | ", "maternal_supplements_short_form_breastfeeding | ", "prams_stressful_life_events_in_pregnancy | ", 
+    "the_everyday_discrimination_scale | ", "promis_depression_8a | ",  "promis_anxiety_8a | ",  "percieved_stress_scale_10_item | ", 
+    "promis_emotional_support_4a | ",  "promis_informational_support_4a | ", "promis_instrumental_support_4a | ", 
+    "promis_general_life_satisfaction_5a | ", "sleep_health_of_adults | ", "household_exposure_to_secondhand_smoke_current | ", 
+    "sleep_health_of_infants | ", "caregiver_health_literacy | ", "couples_satisfaction_index_4_item | "]};
 
     var to_add = ""
 
